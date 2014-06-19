582873f75789aacedd773d23fb6727eb3a773913
Pepe Sosa <Sirius38@MacBook-Air-de-Pepe.local>
2014-06-18 21:44:19 -0500

    Añadida clase prueba

---
+++ Test Git/TLSCommitClass.h
@@ -0,0 +1,13 @@
+//
+//  TLSCommitClass.h
+//  Test Git
+//
+//  Created by Pepe Sosa on 18/06/14.
+//  Copyright (c) 2014 TimeLine Soft. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TLSCommitClass : NSObject
+
+@end
---
+++ Test Git/TLSCommitClass.m
@@ -0,0 +1,13 @@
+//
+//  TLSCommitClass.m
+//  Test Git
+//
+//  Created by Pepe Sosa on 18/06/14.
+//  Copyright (c) 2014 TimeLine Soft. All rights reserved.
+//
+
+#import "TLSCommitClass.h"
+
+@implementation TLSCommitClass
+
+@end
